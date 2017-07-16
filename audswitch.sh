7fea451080344e8a93193a87de0901d0ee995cdf
raysinensis <raysinensis@gmail.com>
2017-07-16 14:23:40 -0400

    now working with hdmi switching

--- audswitch.sh
+++ audswitch.sh
@@ -1,18 +1,30 @@
 #! /bin/bash
+#might need to kill if HDMI not working
+#pulseaudio -k
 #get list of ports
 #pacmd list-sinks | awk '/device.icon_name/ {print}'
-
-activeport=$(pacmd list-sinks | awk '/active port: <analog-output-headphones>/ {print}')
-echo $activeport
+listofsinks=$(pacmd list-sinks | awk '/index/ {print}')
+echo "$listofsinks"
+#activeport=$(echo "$listofsinks" | grep -e '* index: [0-9]')
+echo "$activeport"
 
 case "$activeport" in
-	*headphone*)
-		echo "currently on headphone output, switching now..."
-		pactl set-sink-port 1 analog-output-speaker
+	*0*)
+		echo "currently on speaker output, switching now..."
+		pacmd set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
+		newSink="alsa_output.pci-0000_00_1f.3.analog-stereo"
 		;;
 	*)
-		echo "currently on speaker output, switching now..."
-		pactl set-sink-port 1 analog-output-headphones
+		echo "currently on headphone output, switching now..."
+		pacmd set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1
+		newSink="alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1"
 		;;
-esac
+esac	
+
+pactl list short sink-inputs|while read stream; do
+    streamId=$(echo $stream|cut '-d ' -f1)
+    echo "moving stream $streamId to $newSink"
+    pactl move-sink-input "$streamId" "$newSink"
+done
+
 	
